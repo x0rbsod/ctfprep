211ba1fc58347c571b3d2efd266d6711ee8bfdab
x0rbsod <49502536+x0rbsod@users.noreply.github.com>
2019-04-17 20:19:49 -0400

    Fix some commands and add PEDA

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ cp ./db.txt ~/.env
 #  basic setup                   #
 # ------------------------------ #
 apt clean && apt update && apt upgrade -y
-apt install -y terminator git python-setuptools
+apt install -y terminator git python-setuptools ltrace gcc-multilib strace 
 
 # ------------------------------ #
 #  tools                         #
@@ -29,54 +29,48 @@ apt install -y terminator git python-setuptools
 apt install metasploit-framework
 
 # impacket
-cd /opt
-git clone https://github.com/CoreSecurity/impacket.git
+git clone https://github.com/CoreSecurity/impacket.git /opt/impacket
 cd /opt/impacket && python setup.py install
 
 # fuzzdb
-cd /opt
-git clone https://github.com/fuzzdb-project/fuzzdb.git
+git clone https://github.com/fuzzdb-project/fuzzdb.git /opt/fuzzdb
 
 # empire (run setup manually)
-cd /opt
-git clone https://github.com/PowerShellEmpire/Empire.git
+git clone https://github.com/PowerShellEmpire/Empire.git /opt/Empire
 
 # veil
-cd /opt
-git clone https://github.com/Veil-Framework/Veil.git
-./config/setup.sh --force --silent
+git clone https://github.com/Veil-Framework/Veil.git /opt/Veil
+/opt/veil/config/setup.sh --force --silent
 
 # powersploit
-cd /opt
-git clone https://github.com/PowerShellMafia/PowerSploit.git
+git clone https://github.com/PowerShellMafia/PowerSploit.git /opt/PowerSploit
 
 # searchsploit
 apt -y install exploitdb exploitdb-papers exploitdb-bin-sploits
 searchsploit -u
 
 # windows local enumration
-cd /opt
-git clone https://github.com/bitsadmin/miscellaneous
-mv miscellaneous localrecon.cmd 
+git clone https://github.com/bitsadmin/miscellaneous /opt/localrecon.cmd
 
 # windows exploit suggester
-cd /opt
-git clone https://github.com/GDSSecurity/Windows-Exploit-Suggester
+git clone https://github.com/GDSSecurity/Windows-Exploit-Suggester /opt/Windows-Exploit-Suggester
 pip install xlutils
 cd /opt/Windows-Exploit-Suggester
 python windows-exploit-suggester.py --update
-cd /opt
-git clone https://github.com/bitsadmin/wesng
+git clone https://github.com/bitsadmin/wesng /opt/wesng
 cd /opt/wesng
 python wes.py --update
 
 # nix exploit/enumration
-cd /opt
-git clone https://github.com/InteliSecureLabs/Linux_Exploit_Suggester
-git clone https://github.com/pentestmonkey/unix-privesc-check
+git clone https://github.com/InteliSecureLabs/Linux_Exploit_Suggester /opt/Linux_Exploit_Suggester
+git clone https://github.com/pentestmonkey/unix-privesc-check /opt/unix-privesc-check
 
 # oletools
 apt install python-oletools
+
+# PEDA binary analysis
+git clone https://github.com/longld/peda.git /opt/peda
+echo "source /opt/peda/peda.py" >> ~/.gdbinit
 
 # ------------------------------ #
 #  finish up                     #
